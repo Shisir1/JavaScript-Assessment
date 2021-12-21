c066d9c73ca7d9c86e265ed495a54c3b67d8f024
Shisir1 <shisirhumagain0@gmail.com>
2021-12-20 22:28:05 -0500

    array mehtod and hoist

--- array.js
+++ array.js
@@ -34,3 +34,32 @@ const fruits = ['Banana','Apple','Orange']
 for(i = fruits.length-1; i >=0; i--){
   console.log(fruits[i])
 }
+
+
+const d = new Date(2020,10,12);
+console.log(d)
+document.getElementById('datObj').innerHTML = d;
+
+const date2 = new Date();
+console.log(date2.getDate());
+
+setTimeout(function(){
+  console.log('Shisir')
+},2000)
+
+setTimeout(sayHi,3000){
+  var interval = setInterval(function(
+  console.log('Shisir');
+  )
+},2000)
+clearInterval(interval)
+
+for(var i = 0; i < 4; i++){
+  setTimeout(function(){console.log(i)},1000)
+}
+
+//hoisting
+console.log(hoist)
+var  hoist;
+
+
