a960e68676f43bea331f35607745394d4a9cf08b
Shisir1 <shisirhumagain0@gmail.com>
2021-12-21 22:37:00 -0500

    Array methods an dlet va vs var vs const

--- array.js
+++ array.js
@@ -62,4 +62,95 @@ for(var i = 0; i < 4; i++){
 console.log(hoist)
 var  hoist;
 
+const ab = 2; // the value of const variable can not be changed
 
+{
+  let x = 1;
+}
+console.log(x) //not accessable outside the box because of scope of let
+
+
+
+{
+  x = x + 1
+  let x;
+  console.log(x)  //ReferenceError because it can't be assigned before declaration 
+}
+
+var x = 4;
+{
+  if(x){
+    x = x+1
+    console.log(x)  //output = 5
+  }
+  let x; 
+  console.log(x) //output = 4
+}
+console.log(x)  //output = 4
+
+let count = 4;
+let count = 5;  //error
+
+function foo(n){
+  if(n == 6){
+    let num = 2;
+    //chenged to var to avoid error
+  }
+  console.log(num); //ReferenceError
+}
+
+var n = 6;
+console.log(n);  //prints 6
+
+for(var i =0; i < 5; i++){
+  console.log(i)
+}
+console.log(i)  //prints 5
+for(let j = 0; j< 5; j++){
+  console.log(j) //ReferenceError
+}
+
+const arr = [1,2,3,4];
+arr = [5,6] //TypeError
+
+const arr = [1,2,3,4];
+arr.push(5,6) //Is possible because its not re-assignment but all operation are possible
+
+
+{
+  let x = 4;
+  {
+    let y = 6;
+    console.log(x) // it will print 4 because x variable is global for child block
+  }
+}
+
+//ES6
+var numbers = [30,40,50];
+numbers.map(function(value,i){
+  console.log(value,i) //prints all elements of the array
+  //value is required but index and array are optional
+})
+
+
+function sum(a,b){
+  var total = 0;
+  total = a + b;
+  return total;
+}
+console.log(sum(2,3))
+
+//Arrow functions 
+let sum = (a,b) => a + b;
+console.log(sum(2,3))
+
+//Arrow 2nd way
+let sumArrow = (a,b) => {
+  let total = 0;
+  total = a + b;
+  return total;
+}
+console.log(sumArrow(2,4))
+
+const ages = [32,33,16,28];
+const 
