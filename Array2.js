a7d310c27f28802c3cfdcff0f1088258190a1d55
Shisir1 <shisirhumagain0@gmail.com>
2021-12-26 22:39:25 -0500

    new files

--- Array2.js
+++ Array2.js
@@ -1,7 +1,7 @@
 
 const numbers1 = [175,50,25];
 
-document.getElementById("demo").innerHTML = numbers1.reduce(myFunc);
+//document.getElementById("demo").innerHTML = numbers1.reduce(myFunc);
 
 function myFunc(total,num){
     return total - num;
@@ -12,12 +12,12 @@ let shoppingCart = [
                     {product: 'Memory Card',qty: 2,price: 20.99}
                     ];
 
-const total1 = shoppingCart.reduce(total1,b) => total1 + b.price,0)
+const total1 = shoppingCart.reduce((total1,b) => total1 + b.price,0)
 console.log(total1)
 
 
 //Objects 
-let shoppingCart = [
+let shoppingCart2 = [
                     {product: 'phone',qty: 1,price: 699},
                     {product: 'Screen Protector',qty: 1,price: 9.98},
                     {product: 'Memory Card',qty: 2,price: 20.99}
@@ -50,6 +50,69 @@ const itemsCopy = {...items};
 // [...arr]
 console.log(itemsCopy)
 itemsCopy.product = 'iphone'
+
+let user = {};
+alert(user.noproperty === undefined);
+
+//key in object
+user = {age: 30,isAdmin: true, };
+alert ("age" in user)
+
+//for in loop
+for(let ky in user){
+    alert(ky)
+    console.log(ky)
+}
+
+let m = {
+    width: 200,
+    height:300,
+    title: "my menu"
+}
+//duplicate above object m
+let clone = {};
+for(let k in m){
+    clone[k] = m[k]
+}
+clone.title = "diff"
+console.log(Object.assign(clone,{isShow: true},{age:40}))
+//using spread operator
+console.log({...clone, isShow: false, fulldetails: 'Shisir Humagain'})
+
+//object.entries will return Keys
+//object.values will values 
+console.log(Object.entries(m))
+console.log(Object.values(m))
+
+//Object(hasOwn) to check if Object has specific properties or not
+let objc = [{'ag': 5},{'fn': 'Shisir'},{'fn':'vv'}]
+objc.prop = 'exists';
+if(Object.hasOwn(objc,'prop')){
+    console.log('\'prop\'is owned')
+}
+
+
+function multiplyOnlyNumeric(m){
+    for(let key in m){
+        if(typeof m[key] ==='number'){
+            m[key] = m[key]*2
+        }
+    }
+}
+multiplyOnlyNumeric(m)
+console.log(m)
+
+//compare obj
+let f = {};
+let b = f;
+console.log(f == b)
+console.log(f === b)
+
+//two independent objects are not equal even if they are same or they look same
+let d = {};
+let e = {};
+//obove two objects are not same
+
 
 function sorting(){
     let sort = false;
@@ -85,6 +148,72 @@ characters.forEach((c,index) => {
 let finalFilterResult = characters.filter((c,index) => {
     return characters.indexOf(c) === index;
 
-)}
+})
 console.log(finalFilterResult)
+
+//call, apply and bind
+//this keyword
+function greet(){
+    return `Hello ${this.name}`
+}
+const person1 = {
+    name: 'Shisir Humagain'
+}
+//call and apply need to be applied instantly unlik bind
+console.log(greet.call(person1));
+console.log(greet.apply(person1));
+
+//bind can be stored in a variable and call anytime later on
+const greet2 = greet.bind(person1);
+console.log(greet2())
+
+//Application of call apply and bind
+
+// closure
+
+function getName(name){
+    var branch = "CS"
+    //lixical Scoping = accessing varible in inner function of parent function
+    return function (age){
+        console.log(`${name} is ${age} years old today and branch is ${branch}`)
+    }
+}
+const getBio = getName('Jacob Milne')
+console.log(getBio(6))
+
+//multiply(4)(5)(6) = 120
+//currying
+function multiply(x){
+    return function(y){
+        return function(z){
+        return x * y * z
+        }
+    }
+}
+console.log(multiply(4)(5)(6))
+
+function getMultiply(firstDigit){
+    var secondDigit = 5
+    return function(thirdDigit){
+        var result = firstDigit * secondDigit * thirdDigit
+        console.log(`Multiplication of ${firstDigit}, ${secondDigit} and ${thirdDigit} is ${result} `)
+    }
+}
+const getFirstMultiply = getMultiply(4)
+console.log(getFirstMultiply(6))
+
+//rest operator
+//{...obj}
+// [...arr]
+
+//for...of loop
+function sumofNum(...args){
+    console.log(...args)
+    let total = 0;
+    for(let arg of args){
+        total = total + arg
+    }
+    return total;
+}
+sumofNum(2,3,4,5,6,7,8,9)
 
---
+++ callApply.js
@@ -0,0 +1,72 @@
+//call, apply and bind
+//this keyword
+function greet(){
+  return `Hello ${this.name}`
+}
+const person1 = {
+  name: 'Shisir Humagain'
+}
+//call and apply need to be applied instantly unlik bind
+console.log(greet.call(person1));
+console.log(greet.apply(person1));
+
+//bind can be stored in a variable and call anytime later on
+const greet2 = greet.bind(person1);
+console.log(greet2())
+
+//Example 2 for call vs apply
+var person2 = {
+  name: "Marvin",
+  age: 42,
+  size: '2xM'
+}
+var person3 = {
+  name: "John",
+  age: 43,
+  size: '3xS'
+}
+var sayHello = function(){
+  console.log("Hello " + this.name)
+}
+var sayGoodBye = function(){
+  console.log("GoodBye " + this.name)
+}
+sayHello.apply(person2)
+sayGoodBye.apply(person3)
+
+sayHello.call(person2)
+sayGoodBye.call(person3)
+//Application of call apply and bind
+
+// closure
+
+function getName(name){
+  var branch = "CS"
+  //lixical Scoping = accessing varible in inner function of parent function
+  return function (age){
+      console.log(`${name} is ${age} years old today and branch is ${branch}`)
+  }
+}
+const getBio = getName('Jacob Milne')
+console.log(getBio(6))
+
+//multiply(4)(5)(6) = 120
+//currying
+function multiply(x){
+  return function(y){
+      return function(z){
+      return x * y * z
+      }
+  }
+}
+console.log(multiply(4)(5)(6))
+
+function getMultiply(firstDigit){
+  var secondDigit = 5
+  return function(thirdDigit){
+      var result = firstDigit * secondDigit * thirdDigit
+      console.log(`Multiplication of ${firstDigit}, ${secondDigit} and ${thirdDigit} is ${result} `)
+  }
+}
+const getFirstMultiply = getMultiply(4)
+console.log(getFirstMultiply(6))
